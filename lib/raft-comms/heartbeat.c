909e4c70ffcb6ba58c821b902a59b547806921fd
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-04-26 07:05:01 +0200

    added base heartbeat datastruct and print function

---
+++ lib/raft-comms/heartbeat.c
@@ -0,0 +1,25 @@
+//
+// Created by zheraan on 26/04/23.
+//
+
+#include "heartbeat.h"
+
+void print_hb(heartbeat_s *hb, FILE *stream) {
+    fprintf(stream, "host_id:       %d\n"
+                    "status:        %d\n"
+                    "flags:         %d\n"
+                    "next_index:    %ld\n"
+                    "rep_index:     %ld\n"
+                    "match_index:   %ld\n"
+                    "commit_index:  %ld\n"
+                    "term:          %d\n",
+            hb->host_id,
+            hb->status,
+            hb->flags,
+            hb->next_index,
+            hb->rep_index,
+            hb->match_index,
+            hb->commit_index,
+            hb->term);
+    return;
+}
--- lib/raft-comms/heartbeat.h
+++ lib/raft-comms/heartbeat.h
@@ -5,4 +5,24 @@
 #ifndef THESIS_CSEC_HEARTBEAT_H
 #define THESIS_CSEC_HEARTBEAT_H
 
+#include <stdint.h>
+#include <stdio.h>
+#include "../hosts-list/host.h"
+#include "../data-op.h"
+
+typedef struct heartbeat_s {
+    uint32_t host_id;
+    enum host_status status;
+    int flags;
+
+    uint64_t next_index;
+    uint64_t rep_index;
+    uint64_t match_index;
+    uint64_t commit_index;
+    uint32_t term;
+} heartbeat_s;
+
+// Outputs the state of the structure to the specified output
+void print_hb(heartbeat_s *hb, FILE *stream);
+
 #endif //THESIS_CSEC_HEARTBEAT_H
