b9bb9dc307f521ed1a23ff2d62612e11734d817f
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-04-26 07:07:29 +0200

    refactoring

--- lib/raft-entry-log/log-entry.h
+++ lib/raft-entry-log/log-entry.h
@@ -5,18 +5,20 @@
 #ifndef THESIS_CSEC_LOG_ENTRY_H
 #define THESIS_CSEC_LOG_ENTRY_H
 
-#define ENTRY_STATE_CREATED 1
-#define ENTRY_STATE_QUEUED 2
-#define ENTRY_STATE_PENDING 3
-#define ENTRY_STATE_COMMITTED 4
-
 #include <stdint.h>
 #include "../data-op.h"
 #include "../hosts-list/hosts-list.h"
 
+enum entry_state{
+    ENTRY_STATE_CREATED = 1,
+    ENTRY_STATE_QUEUED = 2,
+    ENTRY_STATE_PENDING = 3,
+    ENTRY_STATE_COMMITTED = 4
+};
+
 typedef struct log_entry_s {
     uint32_t term; // P-Term the entry was proposed in
-    uint8_t state; // Refer to the ENTRY_STATE_* macros
+    enum entry_state state;
 
     uint16_t server_rep[HOSTS_LIST_SIZE]; // ID of server hosts where it is replicated
     uint16_t master_rep[HOSTS_LIST_SIZE]; // ID of master hosts where it is replicated
--- lib/raft-entry-log/log.h
+++ lib/raft-entry-log/log.h
@@ -11,15 +11,13 @@
 typedef struct log_s {
     uint64_t next_index; // index of next log entry that will be added
     uint64_t rep_index; // index of latest log entry that is either pending, committed or applied
-    uint64_t match_index; // index of last log entry replicated by all nodes
+    uint64_t match_index; // index of last log entry replicated by a majority of nodes
     uint64_t commit_index; // index of last log committed locally
 
     uint32_t P_term; // current P-term
     uint32_t HS_term; // current HS-term
 
     log_entry_s entries[LOG_LENGTH]; // Arrays of entries in the log. Defined as static array for performance.
-
-    // TODO snapshot version
 } log_s;
 
 #endif //RAFT_ENTRY_LOG_LIBRARY_H
