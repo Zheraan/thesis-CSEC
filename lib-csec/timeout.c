279249aa4bb2dfa2c55e25618d0e88cfecb185ab
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-05-06 01:01:14 +0200

    Timeout generator changes, acks and propositions now have fixed (depending on defined values) timeouts

--- lib-csec/timeout.c
+++ lib-csec/timeout.c
@@ -4,7 +4,7 @@
 
 #include "timeout.h"
 
-struct timeval timeout_random(enum timeout_type type) {
+struct timeval timeout_gen(enum timeout_type type) {
     struct timeval ntv;
 
     int buf, modulo_sec = 0, modulo_usec = 0;
@@ -26,18 +26,14 @@ struct timeval timeout_random(enum timeout_type type) {
             break;
 
         case TIMEOUT_TYPE_PROPOSITION:
-            if (TIMEOUT_RANGE_PROPOSITION_USEC > 0)
-                modulo_usec = TIMEOUT_RANGE_PROPOSITION_USEC + 1;
-            if (TIMEOUT_RANGE_PROPOSITION_SEC > 0)
-                modulo_sec = TIMEOUT_RANGE_PROPOSITION_SEC + 1;
-            break;
+            ntv.tv_usec = TIMEOUT_VALUE_PROPOSITION_USEC;
+            ntv.tv_sec = TIMEOUT_VALUE_PROPOSITION_SEC;
+            return ntv;
 
         case TIMEOUT_TYPE_ACK:
-            if (TIMEOUT_RANGE_ACK_USEC > 0)
-                modulo_usec = TIMEOUT_RANGE_ACK_USEC + 1;
-            if (TIMEOUT_RANGE_ACK_SEC > 0)
-                modulo_sec = TIMEOUT_RANGE_ACK_SEC + 1;
-            break;
+            ntv.tv_usec = TIMEOUT_VALUE_ACK_USEC;
+            ntv.tv_sec = TIMEOUT_VALUE_ACK_SEC;
+            return ntv;
 
         case TIMEOUT_TYPE_ELECTION:
             if (TIMEOUT_RANGE_ELECTION_USEC > 0)
@@ -53,24 +49,31 @@ struct timeval timeout_random(enum timeout_type type) {
                 modulo_sec = TIMEOUT_RANGE_FUZZER_SEC + 1;
             break;
 
+        case TIMEOUT_TYPE_RANDOM_OPS:
+            if (TIMEOUT_RANGE_RANDOM_OPS_USEC > 0)
+                modulo_usec = TIMEOUT_RANGE_RANDOM_OPS_USEC + 1;
+            if (TIMEOUT_RANGE_RANDOM_OPS_SEC > 0)
+                modulo_sec = TIMEOUT_RANGE_RANDOM_OPS_SEC + 1;
+            break;
+
         default:
             fprintf(stderr, "Unknown timeout type\n");
             errno = EUNKOWN_TIMEOUT_TYPE;
             return ntv;
     }
 
-    if (modulo_usec != 0) {
+    if (modulo_usec > 0) {
         evutil_secure_rng_get_bytes(&buf, sizeof(int));
-        buf %= modulo_usec; // Ensure value is no bigger than the alphanumeric range
-        ntv.tv_usec = buf + TIMEOUT_OFFSET; // Ensure value is within the alphanumeric range
+        buf %= modulo_usec; // Set the value inside the range
+        ntv.tv_usec = buf + TIMEOUT_OFFSET_USEC; // Add the offset
     } else {
         ntv.tv_usec = 0;
     }
 
-    if (modulo_sec != 0) {
+    if (modulo_sec > 0) {
         evutil_secure_rng_get_bytes(&buf, sizeof(int));
-        buf %= modulo_sec; // Ensure value is no bigger than the alphanumeric range
-        ntv.tv_sec = buf + TIMEOUT_OFFSET; // Ensure value is within the alphanumeric range
+        buf %= modulo_sec; // Set the value inside the range
+        ntv.tv_sec = buf + TIMEOUT_OFFSET_SEC; // Add the offset
     } else {
         ntv.tv_sec = 0;
     }
--- lib-csec/timeout.h
+++ lib-csec/timeout.h
@@ -11,25 +11,21 @@
 
 #define EUNKOWN_TIMEOUT_TYPE (-1)
 
-#ifndef TIMEOUT_OFFSET
-#define TIMEOUT_OFFSET 100 // minimum value of timeouts, maximum is offset + 
+#ifndef TIMEOUT_OFFSET_USEC
+#define TIMEOUT_OFFSET_USEC 100 // minimum value of timeouts' timeval's usec value, maximum is offset + range
 #endif
 
-// Defines for the usec value of timeouts' timeval
+#ifndef TIMEOUT_OFFSET_SEC
+#define TIMEOUT_OFFSET_SEC 0 // minimum value of timeouts' timeval's sec value, maximum is offset + range
+#endif
+
+// Defines for the range of the usec value in timeouts' timeval
 #ifndef TIMEOUT_RANGE_P_HB_USEC
 #define TIMEOUT_RANGE_P_HB_USEC 500
 #endif
 
 #ifndef TIMEOUT_RANGE_HS_HB_USEC
 #define TIMEOUT_RANGE_HS_HB_USEC 500
-#endif
-
-#ifndef TIMEOUT_RANGE_PROPOSITION_USEC
-#define TIMEOUT_RANGE_PROPOSITION_USEC 500
-#endif
-
-#ifndef TIMEOUT_RANGE_ACK_USEC
-#define TIMEOUT_RANGE_ACK_USEC 500
 #endif
 
 #ifndef TIMEOUT_RANGE_ELECTION_USEC
@@ -40,21 +36,17 @@
 #define TIMEOUT_RANGE_FUZZER_USEC 0
 #endif
 
-// Defines for the sec value of timeouts' timeval
+#ifndef TIMEOUT_RANGE_RANDOM_OPS_USEC
+#define TIMEOUT_RANGE_RANDOM_OPS_USEC 0
+#endif
+
+// Defines for the range of the sec value in timeouts' timeval
 #ifndef TIMEOUT_RANGE_P_HB_SEC
 #define TIMEOUT_RANGE_P_HB_SEC 0
 #endif
 
 #ifndef TIMEOUT_RANGE_HS_HB_SEC
 #define TIMEOUT_RANGE_HS_HB_SEC 0
-#endif
-
-#ifndef TIMEOUT_RANGE_PROPOSITION_SEC
-#define TIMEOUT_RANGE_PROPOSITION_SEC 0
-#endif
-
-#ifndef TIMEOUT_RANGE_ACK_SEC
-#define TIMEOUT_RANGE_ACK_SEC 0
 #endif
 
 #ifndef TIMEOUT_RANGE_ELECTION_SEC
@@ -65,18 +57,41 @@
 #define TIMEOUT_RANGE_FUZZER_SEC 1
 #endif
 
+#ifndef TIMEOUT_RANGE_RANDOM_OPS_SEC
+#define TIMEOUT_RANGE_RANDOM_OPS_SEC 5
+#endif
+
+// Defines for the fixed timeouts values
+#ifndef TIMEOUT_VALUE_PROPOSITION_USEC
+#define TIMEOUT_VALUE_PROPOSITION_USEC 500
+#endif
+
+#ifndef TIMEOUT_VALUE_PROPOSITION_SEC
+#define TIMEOUT_VALUE_PROPOSITION_SEC 0
+#endif
+
+#ifndef TIMEOUT_VALUE_ACK_SEC
+#define TIMEOUT_VALUE_ACK_SEC 0
+#endif
+
+#ifndef TIMEOUT_VALUE_ACK_USEC
+#define TIMEOUT_VALUE_ACK_USEC 500
+#endif
+
 enum timeout_type{
-    TIMEOUT_TYPE_P_HB = 0,
-    TIMEOUT_TYPE_HS_HB = 1,
-    TIMEOUT_TYPE_PROPOSITION = 2,
-    TIMEOUT_TYPE_ACK = 3,
-    TIMEOUT_TYPE_ELECTION = 4,
-    TIMEOUT_TYPE_FUZZER = 5,
+    TIMEOUT_TYPE_P_HB = 0, // P master heartbeat timeout type
+    TIMEOUT_TYPE_HS_HB = 1, // HS master heartbeat timeout
+    TIMEOUT_TYPE_PROPOSITION = 2, // Data op proposition timeout type
+    TIMEOUT_TYPE_ACK = 3, // Message ack timeout type
+    TIMEOUT_TYPE_ELECTION = 4, // Master election timeout type
+    TIMEOUT_TYPE_FUZZER = 5, // Fuzzer delay introduction timeout type
+    TIMEOUT_TYPE_RANDOM_OPS = 6, // Random data op generator timeout type
 };
 
-// Returns a timeval containing a random value within the defined parameters
-// of the range corresponding to the timeout type.
-// Defined parameters may be redefined at compile time
-struct timeval timeout_random(enum timeout_type type);
+// In case of success, returns a timeval containing a value within the defined parameters, either a random value
+// in a range or a fixed time, depending on the timeout type specified in the function parameter.
+// In case of unknown timeval range type, returns an uninitialized timeval and errno is set to EUNKOWN_TIMEOUT_TYPE.
+// Defined parameters (see above) may be redefined at compile time.
+struct timeval timeout_gen(enum timeout_type type);
 
 #endif //THESIS_CSEC_TIMEOUT_H
