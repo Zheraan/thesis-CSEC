07e4178244d8c2649cf7afb2a33e980a8d0c4b36
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-05-11 08:40:48 +0200

    Added security check for mfs_free_cache

--- lib-csec/mocked-fs/mocked-fs.c
+++ lib-csec/mocked-fs/mocked-fs.c
@@ -27,10 +27,14 @@ int mfs_apply_op(mocked_fs_s *mfs, data_op_s *op) {
 }
 
 void mfs_free_cache(overseer_s *overseer) {
+    if (overseer->mfs == NULL)
+        return;
     free(overseer->mfs->op_cache);
-    overseer->mfs->op_cache = NULL;
-    event_del(overseer->mfs->event_cache);
-    event_free(overseer->mfs->event_cache);
+    overseer->mfs->op_cache = NULL; // Avoid dangling pointer when reusing
+    if (overseer->mfs->event_cache != NULL) {
+        event_del(overseer->mfs->event_cache);
+        event_free(overseer->mfs->event_cache);
+    }
     overseer->mfs->retransmission_attempts = 0;
     return;
 }
