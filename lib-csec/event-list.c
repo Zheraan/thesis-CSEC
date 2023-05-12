e59397969fff419e3dd68bfb93c76ed2b734e8d2
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-05-12 04:07:29 +0200

    fixed event list add dangling pointers and added debut prints

--- lib-csec/event-list.c
+++ lib-csec/event-list.c
@@ -17,13 +17,23 @@ void event_list_free(event_list_s *el) {
 }
 
 int event_list_add(overseer_s *o, struct event *ev) {
+    if (DEBUG_LEVEL >= 3) {
+        printf("\n    - Creating an event list element ... ");
+        fflush(stdout);
+    }
     if (o->el == NULL) {
         o->el = malloc(sizeof(event_list_s));
         if (o->el == NULL) {
-            perror("Malloc");
+            perror("Malloc event list elem");
+            fflush(stderr);
             return EXIT_FAILURE;
         }
+
         o->el->e = ev;
+        o->el->next = NULL;
+        if (DEBUG_LEVEL >= 3) {
+            printf("Done.\n");
+        }
         return EXIT_SUCCESS;
     }
 
@@ -31,10 +41,20 @@ int event_list_add(overseer_s *o, struct event *ev) {
     do {
         if (next->next == NULL) {
             next->next = malloc(sizeof(event_list_s));
+            if (next->next == NULL) {
+                perror("Malloc event list elem");
+                fflush(stderr);
+                return EXIT_FAILURE;
+            }
             next->next->e = ev;
+            next->next->next = NULL;
             break;
         }
         next = next->next;
     } while (next != NULL);
+
+    if (DEBUG_LEVEL >= 3) {
+        printf("Done.\n");
+    }
     return EXIT_SUCCESS;
 }
