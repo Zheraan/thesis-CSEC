226e5690ad462eb1cbf5cfff31b92fe7c90c81ec
Renaud Jean-Claude Formosa <refor21@student.sdu.dk>
2023-08-02 22:46:46 +0200

    fixed P liveness, refactoring for clarity

--- lib-csec/status/p-liveness.c
+++ lib-csec/status/p-liveness.c
@@ -5,8 +5,11 @@
 #include "p-liveness.h"
 
 int p_liveness_set_timeout(overseer_s *overseer) {
-    if (overseer->p_liveness_event != NULL)
+    if (overseer->p_liveness_event != NULL) {
+        event_free(overseer->p_liveness_event); // Freeing the past event if any
+        overseer->p_liveness_event = NULL; // Clear dangling pointer in case this function fails
         debug_log(4, stdout, "Re-initializing next P Liveness timeout event ... ");
+    }
     else debug_log(4, stdout, "Initializing next P Liveness timeout event ... ");
 
     struct event *p_liveness_event = evtimer_new(overseer->eb,
@@ -20,9 +23,6 @@ int p_liveness_set_timeout(overseer_s *overseer) {
 
     // P Liveness has low priority
     event_priority_set(p_liveness_event, 1);
-
-    if (overseer->p_liveness_event != NULL) // Freeing the past event if any
-        event_free(overseer->p_liveness_event);
     overseer->p_liveness_event = p_liveness_event;
 
     // Add the event in the loop
@@ -42,18 +42,20 @@ void p_liveness_timeout_cb(evutil_socket_t fd, short event, void *arg) {
     debug_log(1, stdout, "P liveness timed out ... ");
     // TODO Improvement Don't monitor liveness when there is no P and local node is not HS
 
+    overseer_s *overseer = (overseer_s *) arg;
+
     // Set P's status as Unreachable
     errno = 0;
-    uint32_t p_id = hl_whois(((overseer_s *) arg)->hl, HOST_STATUS_P);
+    uint32_t p_id = hl_whois(overseer->hl, HOST_STATUS_P);
     if (errno != ENONE) { // If there was an active P node
         debug_log(1, stdout, "P liveness timed out: setting its status as UNREACHABLE.\n");
-        hl_update_status((overseer_s *) arg, HOST_STATUS_UNREACHABLE, p_id);
+        hl_update_status(overseer, HOST_STATUS_UNREACHABLE, p_id);
     }
 
     // If local is HS
-    if (((overseer_s *) arg)->hl->hosts[((overseer_s *) arg)->hl->localhost_id].status == HOST_STATUS_HS)
-        promote_to_p((overseer_s *) arg); // Promote to P
-    else p_liveness_set_timeout((overseer_s *) arg); // Otherwise set the next timeout
+    if (overseer->hl->hosts[overseer->hl->localhost_id].status == HOST_STATUS_HS)
+        promote_to_p(overseer); // Promote to P
+    else p_liveness_set_timeout(overseer); // Otherwise set the next timeout
 
     return;
 }
